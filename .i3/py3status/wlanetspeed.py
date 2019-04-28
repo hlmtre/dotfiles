d266f1bf327df1432e9b88d96e49564f47d06785
Matt Weller <hellmitre@gmail.com>
2019-04-27 23:55:18 -0700

    formatting/display updates * steal a prefix formatting function from github.com/leepro * small formatting fixes otherwise * convert to bps (kbps, mbps - small b bits)

--- .i3/py3status/wlanetspeed.py
+++ .i3/py3status/wlanetspeed.py
@@ -1,8 +1,31 @@
+# -*- coding: utf-8 -*-
 from time import time
 from subprocess import check_output
 
 class Py3status:
   my_interface = "wlp110s0"
+
+  SYMBOLS = {
+    'customary'     : ('b', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb')
+}
+
+  def _bytes2human(self, n, format='%(value).1f %(symbol)s', symbols='customary'):
+    """
+      Cribbed from github.com/leepro
+      https://gist.github.com/leepro/9694638
+    """
+    n = int(n)
+    if n < 0:
+        raise ValueError("n < 0")
+    symbols = self.SYMBOLS[symbols]
+    prefix = {}
+    for i, s in enumerate(symbols[1:]):
+        prefix[s] = 1 << (i+1)*10
+    for symbol in reversed(symbols[1:]):
+        if n >= prefix[symbol]:
+            value = float(n) / prefix[symbol]
+            return format % locals()
+    return format % dict(symbol=symbols[0], value=n)
 
   def netspeed(self):
     if not self._isup(self.my_interface):
@@ -40,5 +63,9 @@ class Py3status:
 
 # if we assume this is being updated every second... it becomes bytes per second.
         if prev_rx == 0 or prev_tx == 0:
-          return interface + ": " +  str(int(rx_bytes) / 1024 ) + "v|^" + str(int(tx_bytes) / 1024) + " kB/s"
-        return interface + ": " + str((int(rx_bytes) - int(prev_rx)) / 1024) + "v|^" + str((int(tx_bytes) - int(prev_tx)) / 1024 ) + " kB/s"
+          return interface + ": " +  self._bytes2human(int(rx_bytes) * 8) + " ↓|↑ " + self._bytes2human(int(tx_bytes) * 8)
+        return interface + ": " + self._bytes2human((int(rx_bytes) - int(prev_rx)) * 8) + " ↓|↑ " + self._bytes2human((int(tx_bytes) - int(prev_tx)) * 8)
+
+if __name__ == "__main__":
+  from py3status.module_test import module_test
+  module_test(Py3status)
