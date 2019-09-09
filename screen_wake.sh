71eef89ef82e5c3d1ce20811e2dc115f1c280c34
Matt Weller <hellmitre@gmail.com>
2019-09-08 22:44:41 -0700

    update for using a DP->HDMI cable after weird driver update

--- screen_wake.sh
+++ screen_wake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xrandr --output HDMI-0 --auto --primary --output DP-3 --mode 3440x1440 --rate 60.00 --left-of HDMI-0 --output DP-0 --off
+xrandr --output DP-4 --auto --primary --output DP-3 --mode 3440x1440 --rate 60.00 --left-of DP-4 --output DP-0 --off
 sleep 5
-xrandr --output HDMI-0 --auto --primary --output DP-3 --mode 3440x1440 --rate 99.99 --left-of HDMI-0 --output DP-0 --off
+xrandr --output DP-4 --auto --primary --output DP-3 --mode 3440x1440 --rate 99.99 --left-of DP-4 --output DP-0 --off
 
 exit 0
