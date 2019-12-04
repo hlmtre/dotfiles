18bb5808b16c916e83145770b3960c88719847fe
Matt Weller <hellmitre@gmail.com>
2019-12-03 17:22:28 -0800

    add wget to neovim.sh dependencies

--- neovim.sh
+++ neovim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES="ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip build-essential curl"
+PACKAGES="ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip build-essential curl wget"
 URL=https://github.com/neovim/neovim/archive/v0.4.2.tar.gz
 
 sudo apt install $PACKAGES
