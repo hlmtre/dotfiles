3eb9524ee1ced3b75c5fa390c516ad1b5df13c5c
Matt Weller <hlmtre@ryzef.lan.zero9f9.com>
2020-07-31 23:37:42 -0700

    add xrandr script two.sh

---
+++ two.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --pos 3440x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3440x1440 --pos 0x0 --rotate normal
