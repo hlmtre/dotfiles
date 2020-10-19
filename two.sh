377180144d141e79e812fbb48221627be64fec2e
Matt Weller <hellmitre@gmail.com>
2020-10-19 11:45:24 -0700

    add observer

--- two.sh
+++ two.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --pos 3440x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3440x1440 --pos 0x0 --rotate normal
+xrandr --output DP-4 --primary --rate 143.91 --mode 2560x1440 --output DP-0 --mode 3440x1440 --rate 100.00 --left-of DP-4
+sleep 5
+i3-resurrect restore -w 1irc
+i3-resurrect restore -w 2web
+i3-resurrect restore -w 3cli
+i3-resurrect restore -w 4vpn
+i3-resurrect restore -w e
