799ef93c3d6e67f868790a94b9ebddba58e68df3
Matt Weller <hellmitre@gmail.com>
2021-02-14 17:02:18 -0800

    add neomuttrc, update sway config, update homemaker

--- libinput.sh
+++ libinput.sh
@@ -6,6 +6,7 @@ if [[ $HOSTNAME = "ryzef.lan.zero9f9.com" ]] ; then
     xinput set-prop 11 "libinput Accel Speed" 0
 elif [[ $HOSTNAME = "fx270.lan.zero9f9.com" ]] ; then
     xinput set-prop 12 "libinput Accel Speed" -0.6
+    xinput disable 11
 else
     xinput set-prop 11 "libinput Accel Speed" -0.6
 fi
