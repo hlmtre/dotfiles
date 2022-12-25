882013c66296536b893f7d8d2d70ef669f62fc09
Matt Weller <hellmitre@gmail.com>
2022-12-25 01:05:35 -0800

    updates for config/styling

--- libinput.sh
+++ libinput.sh
@@ -2,8 +2,9 @@
 
 
 HOSTNAME=`hostname`
-if [[ $HOSTNAME = "ryzef.lan.zero9f9.com" ]] ; then
-    xinput set-prop 11 "libinput Accel Speed" 0
+if [[ $HOSTNAME = "ryzef.lan.zero9f9.com" || $HOSTNAME = "u22.lan.zero9f9.com" ]] ; then
+  echo "u22"
+    xinput set-prop "Logitech Gaming Mouse G502" "libinput Accel Speed" 0.4
 elif [[ $HOSTNAME = "fx270.lan.zero9f9.com" ]] ; then
     xinput set-prop 12 "libinput Accel Speed" -0.6
     xinput disable 11
--- two.sh
+++ two.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-xrandr --output DP-0 --primary --rate 143.91 --mode 2560x1440 --output DP-0 --mode 3440x1440 --rate 100.00 --left-of DP-2
-sleep 5
-i3-resurrect restore -w 1:irc
-i3-resurrect restore -w 2:web
-i3-resurrect restore -w 3:cli
-i3-resurrect restore -w 4:vpn
-i3-resurrect restore -w e
+#xrandr --output DP-0 --primary --rate 143.91 --mode 2560x1440 --output DP-0 --mode 3440x1440 --rate 100.00 --left-of DP-2
+xrandr --output DP-4 --primary --mode 2560x1440 --rate 143.91 --output DP-2 --auto --left-of DP-4
+#sleep 5
+#i3-resurrect restore -w 1:irc
+#i3-resurrect restore -w 2:web
+#i3-resurrect restore -w 3:cli
+#i3-resurrect restore -w 4:vpn
+#i3-resurrect restore -w e
