c20cb89b9c40fed6ef8436a8a4860a1366d410f8
Matt Weller <hellmitre@gmail.com>
2023-02-21 23:27:35 -0800

    trying (and failing) to migrate to lazy.nvim

--- .config/nvim/lua/config/which.lua
+++ .config/nvim/lua/config/which.lua
@@ -1,6 +1,41 @@
-local wk = require("which-key")
+local opts = {
+  mode = "n",
+  prefix = "<leader>",
+  buffer = nil,
+  silent = false,
+  noremap = true,
+  nowait = true,
+}
 
-wk.register({
+local mappings = {
+  ["l"] = {
+    name = "lsp",
+    R = { ":RustRunnables<CR>", "rust runnables" },
+    e = {
+      function()
+        vim.lsp.buf.references()
+      end,
+      "references",
+    },
+    r = { ":IncRename ", "rename" },
+    a = { ":CodeActionMenu<CR>", "code action" },
+    d = {
+      function()
+        vim.lsp.diagnostic.show_line_diagnostics()
+      end,
+      "diagnostics",
+    },
+    D = {
+      function()
+        vim.lsp.buf.definition()
+      end,
+      "definition",
+    },
+    p = {
+      name = { "Lazy<CR>", "Lazy" },
+    },
+    t = { ":TroubleToggle<CR>", "trouble" },
+  },
   f = {
     name = "file",
     f = { "<cmd>Telescope file_browser<cr>", "find file" },
@@ -19,11 +54,6 @@ wk.register({
     b = { "<cmd>Telescope buffers<CR>", "list" },
     l = { "<cmd>b#<CR>", "last" },
   },
-  p = {
-    name = "packer",
-    s = { "<cmd>PackerSync<CR>", "sync" },
-    c = { "<cmd>PackerCompile<CR>", "compile" },
-  },
   g = {
     name = "git",
     s = { "<cmd>Neogit<CR>", "status" },
@@ -31,35 +61,23 @@ wk.register({
   },
   x = {
     name = "autopairs",
-    d = { function() require('nvim-autopairs').disable() end, "disable"},
-    e = { function() require('nvim-autopairs').enable() end, "enable"}
-  }
-}, {
-  prefix = "<leader>",
-})
-
-local opts = {
-  mode = "n",
-  prefix = "<leader>",
-  buffer = nil,
-  silent = false,
-  noremap = true,
-  nowait = true,
-}
-
-local mappings = {
-  ["l"] = {
-    name = "lsp",
-    R = { ":RustRunnables<CR>", "rust runnables" },
-    e = { function() vim.lsp.buf.references() end, "references" },
-    r = { ":IncRename " , "rename" },
-    a = { ":CodeActionMenu<CR>", "code action" },
-    d = { function() vim.lsp.diagnostic.show_line_diagnostics() end, "diagnostics" },
-    D = { function() vim.lsp.buf.definition() end, "definition" },
-    t = { ":TroubleToggle<CR>", "trouble" },
+    d = {
+      function()
+        require("nvim-autopairs").disable()
+      end,
+      "disable",
+    },
+    e = {
+      function()
+        require("nvim-autopairs").enable()
+      end,
+      "enable",
+    },
   },
+  { prefix = "<leader>" },
 }
 
+local wk = require("which-key")
 wk.register(mappings, opts)
 
 local plugins = {}
