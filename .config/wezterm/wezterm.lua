e449f2268281533e7137da17b3e5aee57a1e1fd0
Matt Weller <hellmitre@gmail.com>
2024-04-28 07:44:08 -0700

    nvim and wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,20 +1,21 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 return {
-  color_scheme = 'Gruvbox Dark',
-  font = wezterm.font('DejaVuSansMono Nerd Font'),
-  mouse_bindings = {
-  -- Ctrl-click will open the link under the mouse cursor
-    {
-      event = { Up = { streak = 1, button = 'Left' } },
-      mods = 'CTRL',
-      action = wezterm.action.OpenLinkAtMouseCursor,
-    },
-  },
-    keys = {
-    {
-      key = 'r',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.ReloadConfiguration,
-    },
-  },
+	color_scheme = "Gruvbox Dark (Gogh)",
+	font = wezterm.font("DejaVuSansMono Nerd Font"),
+	font_size = 10.5,
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+	keys = {
+		{
+			key = "r",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ReloadConfiguration,
+		},
+	},
 }
