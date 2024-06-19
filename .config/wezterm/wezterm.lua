27284beb0a6b8e5ef469c715510e25a276552a23
Matt Weller <hellmitre@gmail.com>
2024-06-19 08:01:56 -0700

    update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require("wezterm")
 return {
+	enable_wayland = false,
 	color_scheme = "Gruvbox Dark (Gogh)",
-	font = wezterm.font("DejaVuSansMono Nerd Font"),
-	font_size = 10.5,
+	font = wezterm.font("DejaVuSansM Nerd Font"),
+	font_size = 9.5,
 	mouse_bindings = {
 		-- Ctrl-click will open the link under the mouse cursor
 		{
@@ -16,6 +17,16 @@ return {
 			key = "r",
 			mods = "CTRL|SHIFT",
 			action = wezterm.action.ReloadConfiguration,
+		},
+		{
+			key = "_",
+			mods = "ALT|SHIFT",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "|",
+			mods = "ALT|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 		},
 	},
 }
